85d49bd45580cfc5dc7ebc6de691cd1d62f4b2dd
Nithyadevi <nithi2218@gmail.com>
2022-05-09 13:42:12 +1000

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function About(props) {
+  const bio = props.bio;
   return (
+    
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      { bio ? <p>{bio}</p> : ""}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />      
     </div>
+    
   );
 }
 
---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Links(props) {
+    return (
+        <h3>Links
+            <a href="github">https://github.com/liza</a>
+            <a href="linkedin">https://www.linkedin.com/in/liza/</a>
+        </h3>
+    );
+}
+
+export default Links;
